485b0eed5e8a86af4dccf0fdb4657ad46d8981e1
Galin <galin.kiriakova@gmail.com>
2025-01-14 19:18:16 +0700

    Revert "Revert "Removed unnecessary code.""
    This reverts commit f9bdec70

--- vue-project/src/views/MainPageView.vue
+++ vue-project/src/views/MainPageView.vue
@@ -4,17 +4,25 @@
 
     <div class="my-style">
       <TodoList/>
+
+      <div class="btn-add">
+
+        <Buttons>
+          <router-link to="/create-page">➕</router-link>
+        </Buttons>
+      </div>
+
     </div>
+
   </main>
 </template>
 
 <script>
 import TodoList from "@/components/main/TodoList.vue";
+import Buttons from "@/components/UI/Buttons.vue";
 
 export default {
   name: 'MainPage',
-
-  components: {TodoList},
-
+  components: {TodoList, Buttons},
 }
 </script>
